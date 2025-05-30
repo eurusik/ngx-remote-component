f7faee9a68bc83c2201e4d16722428ce10829697
Eugene Rusakov <john.rusakov@gmail.com>
2025-05-30 21:52:14 +0300

    Add recommendation for ngx-mf-remote-loader in module comments

--- src/lib/remote-component.module.ts
+++ src/lib/remote-component.module.ts
@@ -10,6 +10,9 @@ import { DefaultRemoteComponentLoader } from './default-remote-component-loader.
   providers: [
     {
       provide: REMOTE_COMPONENT_LOADER,
+      // You can use your own custom loader implementation here,
+      // but we recommend using RemoteLoaderBrowser or RemoteLoaderServer
+      // from the 'ngx-mf-remote-loader' package for Module Federation support
       useClass: DefaultRemoteComponentLoader
     }
   ]
